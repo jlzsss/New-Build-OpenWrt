dd1e04ef9aed22915ed7e941c9f002d0afd00a7b
jlzsss <jlz19821031@gmail.com>
2026-08-19 17:40:41 +0800

    替换dockerd文件

--- Immortalwrt/diy-part2.sh
+++ Immortalwrt/diy-part2.sh
@@ -21,17 +21,4 @@ git clone --depth 1 https://github.com/sbwml/packages_lang_golang -b 26.x feeds/
 git clone --depth 1 --filter=blob:none --sparse https://github.com/immortalwrt/packages.git temp-lede && cd temp-lede && git sparse-checkout set net/uwsgi && cd .. && rm -rf feeds/packages/net/uwsgi && mv temp-lede/net/uwsgi feeds/packages/net && rm -rf temp-lede
 git clone --depth 1 --filter=blob:none --sparse https://github.com/coolsnowwolf/packages.git temp-lede && cd temp-lede && git sparse-checkout set lang/rust && cd .. && rm -rf feeds/packages/lang/rust && mv temp-lede/lang/rust feeds/packages/lang && rm -rf temp-lede
 git clone --depth 1 --filter=blob:none --sparse https://github.com/openwrt/packages.git temp-lede && cd temp-lede && git sparse-checkout set lang/lua/lua5.4 && cd .. && rm -rf feeds/packages/lang/lua/lua5.4 && mv temp-lede/lang/lua/lua5.4 feeds/packages/lang/lua/ && rm -rf temp-lede
-
-# Fix dockerd build failure: copy_binaries() in hack/make/binary-daemon
-# tries to cp executables (containerd, docker-init, rootlesskit, etc.) that
-# don't exist in the GitHub runner's PATH, causing:
-#   cp: cannot stat '': No such file or directory
-# Solution: Use sed to comment out copy_binaries call in binary-daemon script
-DOCKERD_PKG="feeds/packages/utils/dockerd"
-if [ -f "${DOCKERD_PKG}/Makefile" ]; then
-  # Insert sed command before ./hack/make.sh binary to patch the binary-daemon script
-  sed -i '/\.\/hack\/make\.sh binary/i\\tsed -i '\''/copy_binaries/s/^/#/'\'' $(PKG_BUILD_DIR)/hack/make/binary-daemon' "${DOCKERD_PKG}/Makefile"
-
-  # Also fix docker-proxy install path if needed
-  sed -i 's|bundles/binary-daemon/docker-proxy|bundles/binary/docker-proxy|g' "${DOCKERD_PKG}/Makefile" 2>/dev/null || true
-fi
+git clone --depth 1 --filter=blob:none --sparse https://github.com/openwrt/packages.git temp-lede && cd temp-lede && git sparse-checkout set utils/dockerd && cd .. && rm -rf feeds/packages/utils/dockerd && mv temp-lede/utils/dockerd feeds/packages/utils && rm -rf temp-lede
--- lede/diy-part2.sh
+++ lede/diy-part2.sh
@@ -32,20 +32,8 @@ rm -rf feeds/small/miniupnpd*
 rm -rf feeds/small/miniupnp*
 git clone --depth 1 --filter=blob:none --sparse https://github.com/immortalwrt/packages.git temp-lede && cd temp-lede && git sparse-checkout set net/uwsgi && cd .. && rm -rf feeds/packages/net/uwsgi && mv temp-lede/net/uwsgi feeds/packages/net && rm -rf temp-lede
 git clone --depth 1 --filter=blob:none --sparse https://github.com/openwrt/packages.git temp-lede && cd temp-lede && git sparse-checkout set lang/lua/lua5.4 && cd .. && rm -rf feeds/packages/lang/lua/lua5.4 && mv temp-lede/lang/lua/lua5.4 feeds/packages/lang/lua/ && rm -rf temp-lede
+git clone --depth 1 --filter=blob:none --sparse https://github.com/openwrt/packages.git temp-lede && cd temp-lede && git sparse-checkout set utils/dockerd && cd .. && rm -rf feeds/packages/utils/dockerd && mv temp-lede/utils/dockerd feeds/packages/utils && rm -rf temp-lede
 
-# Fix dockerd build failure: copy_binaries() in hack/make/binary-daemon
-# tries to cp executables (containerd, docker-init, rootlesskit, etc.) that
-# don't exist in the GitHub runner's PATH, causing:
-#   cp: cannot stat '': No such file or directory
-# Solution: Use sed to comment out copy_binaries call in binary-daemon script
-DOCKERD_PKG="feeds/packages/utils/dockerd"
-if [ -f "${DOCKERD_PKG}/Makefile" ]; then
-  # Insert sed command before ./hack/make.sh binary to patch the binary-daemon script
-  sed -i '/\.\/hack\/make\.sh binary/i\\tsed -i '\''/copy_binaries/s/^/#/'\'' $(PKG_BUILD_DIR)/hack/make/binary-daemon' "${DOCKERD_PKG}/Makefile"
-
-  # Also fix docker-proxy install path if needed
-  sed -i 's|bundles/binary-daemon/docker-proxy|bundles/binary/docker-proxy|g' "${DOCKERD_PKG}/Makefile" 2>/dev/null || true
-fi
 # git clone --depth 1 --filter=blob:none --sparse https://github.com/immortalwrt/packages.git temp-lede && cd temp-lede && git sparse-checkout set libs/libb64 && cd .. && rm -rf feeds/packages/libs/libb64 && mv temp-lede/libs/libb64 feeds/packages/libs && rm -rf temp-lede
 # git clone --depth 1 --filter=blob:none --sparse https://github.com/immortalwrt/packages.git temp-lede && cd temp-lede && git sparse-checkout set net/transmission && cd .. && rm -rf feeds/packages/net/transmission && mv temp-lede/net/transmission feeds/packages/net && rm -rf temp-lede
 # git clone --depth 1 --filter=blob:none --sparse https://github.com/immortalwrt/packages.git temp-lede && cd temp-lede && git sparse-checkout set net/transmission-web-control && cd .. && rm -rf feeds/packages/net/transmission-web-control && mv temp-lede/net/transmission-web-control feeds/packages/net && rm -rf temp-lede
